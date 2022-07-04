e63df19fc647037dbdc7e3a760119067197dfe37
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-07-05 01:51:49 +0700

    Update banner.ts

--- lib/banner.ts
+++ lib/banner.ts
@@ -1,11 +1,36 @@
 export const banner: string = `
 
-██████╗  ██████╗ ████████╗███████╗ █████╗ ██████╗ ██████╗ 
-██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗
-██████╔╝██║   ██║   ██║   ███████╗███████║██████╔╝██████╔╝
-██╔══██╗██║   ██║   ██║   ╚════██║██╔══██║██╔═══╝ ██╔═══╝ 
-██████╔╝╚██████╔╝   ██║   ███████║██║  ██║██║     ██║     
-╚═════╝  ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝     
-                                                          
+     _                      _______                      _
+  _dMMMb._              .adOOOOOOOOOba.              _,dMMMb_
+ dP'  ~YMMb            dOOOOOOOOOOOOOOOb            aMMP~  `Yb
+ V      ~"Mb          dOOOOOOOOOOOOOOOOOb          dM"~      V
+          `Mb.       dOOOOOOOOOOOOOOOOOOOb       ,dM'
+           `YMb._   |OOOOOOOOOOOOOOOOOOOOO|   _,dMP'
+      __     `YMMM| OP'~"YOOOOOOOOOOOP"~`YO |MMMP'     __
+    ,dMMMb.     ~~' OO     `YOOOOOP'     OO `~~     ,dMMMb.
+ _,dP~  `YMba_      OOb      `OOO'      dOO      _aMMP'  ~Yb._
+
+             `YMMMM\`OOOo     OOO     oOOO'/MMMMP'
+     ,aa.     `~YMMb `OOOb._,dOOOb._,dOOO'dMMP~'       ,aa.
+   ,dMYYMba._         `OOOOOOOOOOOOOOOOO'          _,adMYYMb.
+  ,MP'   `YMMba._      OOOOOOOOOOOOOOOOO       _,adMMP'   `YM.
+  MP'        ~YMMMba._ YOOOOPVVVVVYOOOOP  _,adMMMMP~       `YM
+  YMb           ~YMMMM\`OOOOI`````IOOOOO'/MMMMP~           dMP
+   `Mb.           `YMMMb`OOOI,,,,,IOOOO'dMMMP'           ,dM'
+     `'                  `OObNNNNNdOO'                   `'
+                           `~OOOOO~'   TISSUE
+
+
+___   ___      ___          __    __       _______. _______ .______      .______     ______   .___________.
+\  \ /  /     /   \        |  |  |  |     /       ||   ____||   _  \     |   _  \   /  __  \  |           |
+ \  V  /     /  ^  \       |  |  |  |    |   (----`|  |__   |  |_)  |    |  |_)  | |  |  |  | `---|  |----`
+  >   <     /  /_\  \      |  |  |  |     \   \    |   __|  |      /     |   _  <  |  |  |  |     |  |     
+ /  .  \   /  _____  \     |  `--'  | .----)   |   |  |____ |  |\  \----.|  |_)  | |  `--'  |     |  |     
+/__/ \__\ /__/     \__\     \______/  |_______/    |_______|| _| `._____||______/   \______/      |__|     
+                                                                                                           
+                                                                               
+                                       
+- WhatsApp userbot with many functions 
+- Made By https://t.me/JustRex                                                        
 
 `;
