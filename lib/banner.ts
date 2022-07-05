dcda949c7796447998a0e38d7b5245c13ddf0656
krzky <krzky@yahoo.com>
2022-07-05 17:31:51 +0700

    Update banner.ts

--- lib/banner.ts
+++ lib/banner.ts
@@ -8,6 +8,7 @@ export const banner: string = `
 ██╔╝ ██╗██║  ██║    ╚██████╔╝███████║███████╗██║  ██║██████╔╝╚██████╔╝   ██║   
 ╚═╝  ╚═╝╚═╝  ╚═╝     ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝    ╚═╝   
 - Special whatsapp userbot with many functions     
-- Made By : https://t.me/JustRex                                                                       
+- Made By : https://t.me/JustRex
+- Developed By : https://t.me/KayRzky                                                                 
                                                                                                                                                                                                                                                                                
 `;
