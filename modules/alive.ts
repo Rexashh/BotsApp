6a2e72868e011c7a34ca43436c7d897700456457
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-07-05 20:37:37 +0700

    Update alive.ts

--- modules/alive.ts
+++ modules/alive.ts
@@ -14,10 +14,14 @@ export = {
     demo: { isEnabled: true, text: ".alive" },
     async handle(client: Client, chat: proto.IWebMessageInfo, XA: XA, args: string[]): Promise<void> {
         try {
-            client.sendMessage(
+client.sendMessage(
                 XA.chatId,
-                alive.ALIVE_MSG,
-                MessageType.text
+                fs.readFileSync("./images/alivelogo.jpg"),
+                MessageType.image,
+                 {
+                        caption: alive.ALIVEMSG,
+                    }
+                );
             ).catch(err => inputSanitization.handleError(err, client, XA));
         } catch (err) {
             await inputSanitization.handleError(err, client, XA);
